b15492650ff6e42da6a8e01b8e0ed622e7887230
PhysicX10 <gperiodmarketwatch@gmail.com>
2017-01-21 12:31:20 -0500

    Stuff A Whole Lot of Stuff

--- src/WatsonRank.java
+++ src/WatsonRank.java
@@ -2,18 +2,17 @@ public class WatsonRank {
 	public String solrClusterId = "sce46d929b_1d1e_4bd3_bf60_8472e75cb73a";
 	public String username = "64e80095-84de-4d28-9e7d-4f92f0c881d2";
 	public String password = "VcUAcwnPzwvp";
+	public String configName = "example_config";
+	public String collectionName = "example_collection";
 	private RetrieveAndRank service = new RetrieveAndRank();
 
 public WatsonRank ()
 {
-
+	service.setUsernameAndPassword(username, password);
 }
 
-public boolean uploadJson (String filePath)
+public boolean createCollection()
 {
-	
-	service.setUsernameAndPassword(username, password);
-
 	CollectionAdminRequest.Create createCollectionRequest =  new CollectionAdminRequest.Create();
 	createCollectionRequest.setCollectionName("example_collection");
 	createCollectionRequest.setConfigName("example_config");
@@ -22,11 +21,34 @@ public boolean uploadJson (String filePath)
 	CollectionAdminResponse response = createCollectionRequest.process(solrClient);
     if (!response.isSuccess()) {
       System.out.println(response.getErrorMessages());
-      throw new IllegalStateException("Failed to create collection: "
-          + response.getErrorMessages().toString());
+      throw new IllegalStateException("Failed to create collection: " + response.getErrorMessages().toString());
     }
 	System.out.println("Collection created.");
 	System.out.println(response);
+}
+
+public boolean uploadConfig(String filePath, String newConfigName)
+{
+	File configZip = new File(filePath);
+	service.uploadSolrClusterConfigurationZip(solrClusterId, newConfigName, configZip);
+}
+
+public boolean uploadData(String title, String author, String body, String url, int n)
+{
+	SolrInputDocument newdoc = new SolrInputDocument();
+	document.addField("id", n);
+	document.addField("author", author);
+	document.addField("url", url);
+	document.addField("body", body);
+	document.addField("title", title)
+
+	System.out.println("Indexing document...");
+	UpdateResponse addResponse = solrClient.add(collectionName, newdoc);
+	System.out.println(addResponse);
+
+	// Commit the document to the index so that it will be available for searching.
+	solrClient.commit(collectionName);
+	System.out.println("Indexed and committed document.");
 }
 
 public ___ rankWatson()
@@ -43,21 +65,7 @@ public void getClusters()
 	SolrClusterList clusters = service.getSolrClusters();
 	System.out.println(clusters);
 }
-
-
-
-public static String curl(String request) {
-		String output = "";
-		try {
-			Process p = Runtime.getRuntime().exec(request);
-			p.waitFor();
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				output += line + "\n";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return output;
-	}
+public void getConfigs()
+{
+	System.out.println(service.getSolrClusterConfigurations(solrClusterId));
+}
