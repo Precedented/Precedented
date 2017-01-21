4cccabecf58e3562a3872f95b6f0c60e9df8c950
Nick Keirstead <nicholas_keirstead@horacemann.org>
2017-01-21 14:08:54 -0500

    Hella Stuff

--- src/CurlRequestTest.java
+++ src/CurlRequestTest.java
@@ -1,13 +1,36 @@
 import java.io.*;
+import java.util.Map;
+import java.util.Set;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
 
 public class CurlRequestTest {
 	public static void main(String[] args) {
-		System.out.println(curl("curl https://www.courtlistener.com/api/rest/v3/dockets/?court__id=scotus"));
+		String result = curl("https://www.courtlistener.com/api/rest/v3/dockets/?court__id=scotus");
+		//System.out.println(result);
+		JsonObject obj = strToJsonObj(result);
+		System.out.println(obj);
+		Set<Map.Entry<String, JsonElement>> entries = jsonObjToSet(obj);
+		for (Map.Entry<String, JsonElement> entry : entries) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+		//Map.Entry<String, JsonElement> caseArray = entries.
+	}
+	public static JsonObject strToJsonObj(String s) {
+		JsonParser parser = new JsonParser();
+		JsonElement element = parser.parse(s);
+		JsonObject obj = element.getAsJsonObject();
+		return obj;
+	}
+	public static Set<Map.Entry<String, JsonElement>> jsonObjToSet(JsonObject obj) {
+		Set<Map.Entry<String, JsonElement>> entries = obj.entrySet();
+		return entries;
 	}
 	public static String curl(String request) {
 		String output = "";
 		try {
-			Process p = Runtime.getRuntime().exec(request);
+			Process p = Runtime.getRuntime().exec("curl " + request);
 			p.waitFor();
 			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line = null;
--- src/WatsonRank.java
+++ src/WatsonRank.java
@@ -1,118 +1,137 @@
+import java.io.File;
+import java.net.URI;
+
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.*;
+import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.*;
+import java.io.*;
+import org.apache.solr.client.solrj.impl.*;
+import org.apache.http.client.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.auth.*;
+
+
 public class WatsonRank {
-
-
-//<declration>
-	public static final String SOLR_CLUSTER_ID = "sce46d929b_1d1e_4bd3_bf60_8472e75cb73a";
-	public static final String USERNAME = "64e80095-84de-4d28-9e7d-4f92f0c881d2";
-	public static final String PASSWORD = "VcUAcwnPzwvp";
-	public static String configName = "example_config";
-	public static String collectionName = "example_collection";
-	public static String rankerName = "ranker1";
-	private static RetrieveAndRank service = new RetrieveAndRank();
-	private static HttpSolrClient solrClient;
+    //<declration>
+    public static final String SOLR_CLUSTER_ID = "sce46d929b_1d1e_4bd3_bf60_8472e75cb73a";
+    public static final String USERNAME = "64e80095-84de-4d28-9e7d-4f92f0c881d2";
+    public static final String PASSWORD = "VcUAcwnPzwvp";
+    public static String configName = "example_config";
+    public static String collectionName = "example_collection";
+    public static String rankerName = "ranker1";
+    private static RetrieveAndRank service = new RetrieveAndRank();
+    private static HttpSolrClient solrClient;
 //</declration>
 
 
+    //<construction>
+    public WatsonRank() {
+        service.setUsernameAndPassword(USERNAME, PASSWORD);
+    }
 
-//<construction>
-public WatsonRank ()
-{
-	service.setUsernameAndPassword(USERNAME, PASSWORD);
-}
+    private static HttpSolrClient getSolrClient(String uri, String username, String password) {
+        return new HttpSolrClient(service.getSolrUrl(SOLR_CLUSTER_ID), createHttpClient(uri, username, password));
+    }
 
-private static HttpSolrClient getSolrClient(String uri, String username, String password) {
-    return new HttpSolrClient(service.getSolrUrl(SOLR_CLUSTER_ID), createHttpClient(uri, username, password));
-}
+    private static HttpClient createHttpClient(String uri, String username, String password) {
+        final URI scopeUri = URI.create(uri);
 
-private static HttpClient createHttpClient(String uri, String username, String password) {
-    final URI scopeUri = URI.create(uri);
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(new AuthScope(scopeUri.getHost(), scopeUri.getPort()),
+                new UsernamePasswordCredentials(USERNAME, PASSWORD));
 
-    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-    credentialsProvider.setCredentials(new AuthScope(scopeUri.getHost(), scopeUri.getPort()),
-        new UsernamePasswordCredentials(USERNAME, PASSWORD));
-
-    final HttpClientBuilder builder = HttpClientBuilder.create()
-        .setMaxConnTotal(128)
-        .setMaxConnPerRoute(32)
-        .setDefaultRequestConfig(RequestConfig.copy(RequestConfig.DEFAULT).setRedirectsEnabled(true).build())
-        .setDefaultCredentialsProvider(credentialsProvider)
-        .addInterceptorFirst(new PreemptiveAuthInterceptor());
-    return builder.build();
-}
+        final HttpClientBuilder builder = HttpClientBuilder.create()
+                .setMaxConnTotal(128)
+                .setMaxConnPerRoute(32)
+                .setDefaultRequestConfig(RequestConfig.copy(RequestConfig.DEFAULT).setRedirectsEnabled(true).build())
+                .setDefaultCredentialsProvider(credentialsProvider)
+                .addInterceptorFirst(new PreemptiveAuthInterceptor());
+        return builder.build();
+    }
 //</construction>
 
-//<create>
-public void createCollection()
-{
-	CollectionAdminRequest.Create createCollectionRequest =  new CollectionAdminRequest.Create();
-	createCollectionRequest.setCollectionName("example_collection");
-	createCollectionRequest.setConfigName("example_config");
+    //<create>
+    public void createCollection() {
+        CollectionAdminRequest.Create createCollectionRequest = new CollectionAdminRequest.Create();
+        createCollectionRequest.setCollectionName("example_collection");
+        createCollectionRequest.setConfigName("example_config");
 
-	System.out.println("Creating collection...");
-	CollectionAdminResponse response = createCollectionRequest.process(solrClient);
-    if (!response.isSuccess()) {
-      System.out.println(response.getErrorMessages());
-      throw new IllegalStateException("Failed to create collection: " + response.getErrorMessages().toString());
+        System.out.println("Creating collection...");
+        CollectionAdminResponse response = createCollectionRequest.process(solrClient);
+        if (!response.isSuccess()) {
+            System.out.println(response.getErrorMessages());
+            throw new IllegalStateException("Failed to create collection: " + response.getErrorMessages().toString());
+        }
+        System.out.println("Collection created.");
+        System.out.println(response);
     }
-	System.out.println("Collection created.");
-	System.out.println(response);
-}
 
-public void createRanker ()
-{
-	Ranker ranker = service.createRanker("ranker1", "./training_data.csv");
-	System.out.println(ranker);
-}
+    public void createRanker() {
+        Ranker ranker = service.createRanker("ranker1", "./training_data.csv");
+        System.out.println(ranker);
+    }
 //</create>
 
 
-//<upload>
-public boolean uploadConfig(String filePath, String newConfigName)
-{
-	File configZip = new File(filePath);
-	service.uploadSolrClusterConfigurationZip(SOLR_CLUSTER_ID, newConfigName, configZip);
-}
+    //<upload>
+    public boolean uploadConfig(String filePath, String newConfigName) {
+        File configZip = new File(filePath);
+        service.uploadSolrClusterConfigurationZip(SOLR_CLUSTER_ID, newConfigName, configZip);
+    }
 
-public boolean uploadData(String title, String author, String body, String url, int n)
-{
-	SolrInputDocument newdoc = new SolrInputDocument();
-	document.addField("id", n);
-	document.addField("author", author);
-	document.addField("url", url);
-	document.addField("body", body);
-	document.addField("title", title)
+    public boolean uploadData(String title, String author, String body, String url, int n) {
+        SolrInputDocument newdoc = new SolrInputDocument();
+        document.addField("id", n);
+        document.addField("author", author);
+        document.addField("url", url);
+        document.addField("body", body);
+        document.addField("title", title);
 
-	System.out.println("Indexing document...");
-	UpdateResponse addResponse = solrClient.add(collectionName, newdoc);
-	System.out.println(addResponse);
+        System.out.println("Indexing document...");
+        UpdateResponse addResponse = solrClient.add(collectionName, newdoc);
+        System.out.println(addResponse);
 
-	// Commit the document to the index so that it will be available for searching.
-	solrClient.commit(collectionName);
-	System.out.println("Indexed and committed document.");
-}
+        // Commit the document to the index so that it will be available for searching.
+        solrClient.commit(collectionName);
+        System.out.println("Indexed and committed document.");
+    }
 //</upload>
 
-//<functions>
-public QueryResponse queryWatson (String query)
-{
-	service = new RetrieveAndRank();
-	service.setUsernameAndPassword(USERNAME, PASSWORD);
-	solrClient = getSolrClient(service.getSolrUrl(SOLR_CLUSTER_ID), USERNAME, PASSWORD);
-	SolrQuery query = new SolrQuery("*:*");
-	QueryResponse response = solrClient.query("example_collection", query);
-	System.out.println(response);
-}
+    //<functions>
+    public QueryResponse queryWatson(String query) {
+        service = new RetrieveAndRank();
+        service.setUsernameAndPassword(USERNAME, PASSWORD);
+        solrClient = getSolrClient(service.getSolrUrl(SOLR_CLUSTER_ID), USERNAME, PASSWORD);
+        SolrQuery query = new SolrQuery("*:*");
+        QueryResponse response = solrClient.query("example_collection", query);
+        System.out.println(response);
+    }
 //</functions>
 
 
-//<gets>
-public void getClusters()
-{
-	SolrClusterList clusters = service.getSolrClusters();
-	System.out.println(clusters);
-}
-public void getConfigs()
-{
-	System.out.println(service.getSolrClusterConfigurations(SOLR_CLUSTER_ID));
-}
+    //<gets>
+    public void getClusters() {
+        SolrClusterList clusters = service.getSolrClusters();
+        System.out.println(clusters);
+    }
+
+    public void getConfigs() {
+        System.out.println(service.getSolrClusterConfigurations(SOLR_CLUSTER_ID));
+    }
 //</gets>
+
+    public static String curl(String request) {
+        String output = "";
+        try {
+            Process p = Runtime.getRuntime().exec(request);
+            p.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                output += line + "\n";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+}
